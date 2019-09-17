be89bdbba627a66344f77c415d3040465e103cde
Andrew <vlason15@gmail.com>
2019-09-17 09:59:27 +0300

    feat: add manifest

--- manifest-config.js
+++ manifest-config.js
@@ -1,11 +1,11 @@
 const colors = require('./colors');
 
 module.exports = {
-  name: 'Mate Gatsby Starter',
-  short_name: 'Mate Gatsby Starter',
+  name: 'Andrewel',
+  short_name: 'Andrewel',
   start_url: '/',
   background_color: colors.background,
   theme_color: colors.primary,
   display: 'minimal-ui',
-  icon: 'media/icon.png',
+  icon: 'media/Reactvector.svg',
 };
