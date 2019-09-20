e910e81089c46a925834b5584dbac1a0127f2da6
Andrew <vlason15@gmail.com>
2019-09-20 12:21:06 +0300

    Update MouseIcon.js

--- src/components/MouseIcon.js
+++ src/components/MouseIcon.js
@@ -15,7 +15,7 @@ const ScrollLink = styled.span`
   transform: translateX(-50%);
   cursor: pointer;
 
-  @media only screen and (max-width: 400px) {
+  @media only screen and (max-width: 300px) {
     display: none;
   }
 `;
