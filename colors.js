96a668e24a60d2e122515fdaffbc93924af0a1e9
Andrewel <vlason15@gmail.com>
2020-01-31 15:22:22 +0300

    feat: add google analitics

--- colors.js
+++ colors.js
@@ -10,18 +10,3 @@ module.exports = {
   secondaryLight: '#9a67ea',
   secondaryDark: '#320b86',
 };
-/* 
-module.exports = {
-  background: '#FFFFFF',
-  backgroundDark: '#f3e5f5',
-
-  primary: '#7b1fa2',
-  primaryLight: '#ae52d4',
-  primaryDark: '#4a0072',
-
-  secondary: '#f50057',
-  secondaryLight: '#ff5983',
-  secondaryDark: '#bb002f',
-};
-
- */
